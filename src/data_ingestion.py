c5adcf72c441087605d69ae04492c69d4d0b1e90
manish1008iitr <m.k.dangi99@gmail.com>
2026-09-21 21:55:56 +0530

    Added raw files, added params.yaml

---
+++ src/data_ingestion.py
@@ -0,0 +1,4 @@
+import os 
+import pymupdf4llm
+from pathlib import Path
+script_dir = Path(__file__).parent
--- src/frontend/web_ui.py
+++ src/frontend/web_ui.py
@@ -1,12 +1,16 @@
 import streamlit as st
-from backend_llm import Response_generator
+import os 
+# from pathlib import Path
+# file_dir = Path(__file__).parent
+
+from src.generator import Response_generator
 
 response_gen = Response_generator()
 
 st.set_page_config(
-    page_title="LangGraph Chatbot")
+    page_title="SHOPEASE CHATBOT")
 
-st.title("SIMPLE CHATBOT ON UPSC FACTS")
+st.title("WELCOME TO SHOPEASE CHATBOT ")
 
 
 # ***************** TAKING USER INPUT *****************
@@ -16,12 +20,13 @@ user_input = st.text_input("")
 # ***************** DISPLAYING USER INPUT ****************
 
 if user_input:
-    st.write("You asked:", user_input)
+    with st.chat_message("user"):
+        st.write(user_input)
     response = response_gen.generate_response(user_input)
-    st.write("Response:", response.key_themes)
-    st.write("Response:", response.summary)
-    st.write("Response:", response.sentiment)
-    st.write("Response:", response.pros)
-    st.write("Response:", response.cons)
+    st.write(response.key_themes)
+    st.write(response.summary)
+    st.write(response.sentiment)
+    st.write(response.pros)
+    st.write(response.cons)
 
 
